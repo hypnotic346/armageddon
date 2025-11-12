e6f8ae52136375bcf35b7e0125c10aabe55942cf
hyptronic <hypnoticproject@proton.me>
2025-11-11 20:59:58 -0700

    push from mac

--- src/hacks/hitboxes/hitboxes.cpp
+++ src/hacks/hitboxes/hitboxes.cpp
@@ -11,7 +11,7 @@ namespace hacks {
 
     namespace util {
         // thx prevter (https://github.com/EclipseMenu/EclipseMenu/blob/c3f7239decca141d8c8c2d3bdc917ace4f23ecbf/src/hacks/Level/ShowHitboxes.cpp#L131-L234)
-        static void for_each_object(const GJBaseGameLayer *game, const std::function<void(GameObject *ß)> &callback) {
+        static void for_each_object(const GJBaseGameLayer *game, const std::function<void(GameObject *)> &callback) {
             const int count = game->m_sections.empty() ? -1 : game->m_sections.size();
 
             for (int i = game->m_leftSectionIndex; i <= game->m_rightSectionIndex && i < count; ++i) {
@@ -39,6 +39,47 @@ namespace hacks {
     } // namespace util
 
     class $modify(base_game_layer, GJBaseGameLayer) {
+        float getModifiedDelta(float p0) {
+            float m_resumeTimer; // x0
+            double m_extraDelta; // d1
+            float m_timeWarp;    // s2
+            float v6;            // s1
+            double v7;           // d2
+            double v8;           // d1
+            float v9;            // s0
+            double v10;          // d0
+            double v11;          // d1
+            float result;        // s0
+
+            m_resumeTimer = this->m_resumeTimer;
+            if (m_resumeTimer <= 0) {
+                m_timeWarp = this->m_gameState.m_timeWarp;
+                v6 = p0 + this->m_extraDelta;
+                if (m_timeWarp >= 1.0)
+                    goto LABEL_3;
+            LABEL_6:
+                v7 = (float) (m_timeWarp * 0.0041667);
+                goto LABEL_4;
+            }
+            //cocos2d::CCDirector::sharedDirector(m_resumeTimer);
+            m_extraDelta = this->m_extraDelta;
+            m_timeWarp = this->m_gameState.m_timeWarp;
+            --this->m_resumeTimer;
+            v6 = m_extraDelta + 0.0;
+            if (m_timeWarp < 1.0)
+                goto LABEL_6;
+        LABEL_3:
+            v7 = 0.00416666688f;
+        LABEL_4:
+            v8 = v6;
+            v9 = v8 / v7;
+            v10 = v7 * (double) (int) llroundf(v9);
+            v11 = v8 - v10;
+            result = v10;
+            this->m_extraDelta = v11;
+            return result;
+        }
+
         // (@hypnotic) summary: draws hitboxes on player (+ on death)
         void update(float dt) {
             const auto rotate = [](CCPoint p, const CCPoint &pivot, const float angle) -> CCPoint {
@@ -113,7 +154,7 @@ namespace hacks {
                                             col = {1.0f, 0.0f, 0.0f, 1.0f};
 
                                         if (object->m_objectRadius > 0.f) {
-                                            node->drawCircle(origin, radius, {}, 0.5f, col, 24); // segments = 8 * 3 (ngl its to jagged with 8 segs)
+                                            node->drawCircle(origin, radius, {}, 0.5f, col, 24); // segments = 8 * 3 (ngl its too jagged with 8 segs)
                                         } else {
                                             node->drawPolygon(rect_vertices.data(), rect_vertices.size(), {}, 0.5f, col);
                                         }
--- src/hacks/physics.cpp
+++ src/hacks/physics.cpp
@@ -1,19 +1,51 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCApplication.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 #include <settings/settings.hpp>
 
 using namespace geode::prelude;
 
-// Physics or FPS bypasses.
 namespace hacks {
-    class $modify(application, CCApplication) {
-        void setAnimationInterval(double interval) {
-            double ival = interval;
+    class $modify(base_game_layer, GJBaseGameLayer) {
+    public:
+        // float getModifiedDelta(float p0) {
+        //     float m_resumeTimer; // x0
+        //     double m_extraDelta; // d1
+        //     float m_timeWarp;    // s2
+        //     float v6;            // s1
+        //     double v7;           // d2
+        //     double v8;           // d1
+        //     float v9;            // s0
+        //     double v10;          // d0
+        //     double v11;          // d1
+        //     float result;        // s0
 
-            if (settings::get<bool>("physics.unlock_fps"))
-                ival = 1.0 / static_cast<double>(settings::get<float>("physics.fps_amt"));
-
-            return CCApplication::setAnimationInterval(1.0 / 144);
-        }
+        //     m_resumeTimer = this->m_resumeTimer;
+        //     if (m_resumeTimer <= 0) {
+        //         m_timeWarp = this->m_gameState.m_timeWarp;
+        //         v6 = p0 + this->m_extraDelta;
+        //         if (m_timeWarp >= 1.0)
+        //             goto LABEL_3;
+        //     LABEL_6:
+        //         v7 = (float) (m_timeWarp * 0.0041667);
+        //         goto LABEL_4;
+        //     }
+        //     //cocos2d::CCDirector::sharedDirector(m_resumeTimer);
+        //     m_extraDelta = this->m_extraDelta;
+        //     m_timeWarp = this->m_gameState.m_timeWarp;
+        //     --this->m_resumeTimer;
+        //     v6 = m_extraDelta + 0.0;
+        //     if (m_timeWarp < 1.0)
+        //         goto LABEL_6;
+        // LABEL_3:
+        //     v7 = 0.00416666688f;
+        // LABEL_4:
+        //     v8 = v6;
+        //     v9 = v8 / v7;
+        //     v10 = v7 * (double) (int) llroundf(v9);
+        //     v11 = v8 - v10;
+        //     result = v10;
+        //     this->m_extraDelta = v11;
+        //     return result;
+        // }
     };
 } // namespace hacks
